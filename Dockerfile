3991a7a6d75048b0a489800a63913a8380c72795
test_oke <ivan.sampedro@oracle.com>
2020-03-10 07:19:04 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM delabassee/fn-cache:latest as cache-stage
+FROM openjdk:12-oracle as build-stage
+WORKDIR /function
+
+RUN curl https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -o apache-maven-3.6.3-bin.tar.gz 
+RUN tar -zxvf apache-maven-3.6.3-bin.tar.gz
+ENV PATH="/function/apache-maven-3.6.3/bin:${PATH}"
+RUN yum install -y unzip
+
+COPY dbwallet.zip /function
+RUN unzip /function/dbwallet.zip -d /function/wallet/ && rm /function/dbwallet.zip
+
+ENV MAVEN_OPTS -Dhttp.proxyHost= -Dhttp.proxyPort= -Dhttps.proxyHost= -Dhttps.proxyPort= -Dhttp.nonProxyHosts= -Dmaven.repo.local=/usr/share/maven/ref/repository
+ADD pom.xml /function/pom.xml
+ADD src /function/src
+ADD libs/* /function/target/libs/
+
+RUN ["mvn", "package"]
+
+RUN ["mvn", "package", \
+    "dependency:copy-dependencies", \
+    "-DincludeScope=runtime", \
+    "-Dmdep.prependGroupId=true", \
+    "-DoutputDirectory=target","-e"]
+
+#RUN ls -lRa /function
+
+RUN /usr/java/openjdk-12/bin/jlink --compress=2 --no-header-files --no-man-pages --strip-debug --output /function/fnjre --add-modules $(/usr/java/openjdk-12/bin/jdeps --ignore-missing-deps --print-module-deps --class-path '/function/target/*' /function/target/function.jar)
+
+FROM oraclelinux:8-slim
+WORKDIR /function
+
+COPY --from=build-stage /function/target/*.jar /function/
+COPY --from=build-stage /function/fnjre/ /function/fnjre/
+#COPY --from=build-stage /function/target/ /function/target/
+COPY --from=build-stage /function/wallet/ /function/wallet/
+COPY --from=cache-stage /libfnunixsocket.so /lib
+
+ENTRYPOINT [ "/function/fnjre/bin/java", \
+    "--enable-preview", \
+    "-cp", "/function/*", \
+    "com.fnproject.fn.runtime.EntryPoint" ]
+
+#ENTRYPOINT [ "/usr/bin/java","-XX:+UseSerialGC","--enable-preview","-Xshare:on","-cp", "/function/*","com.fnproject.fn.runtime.EntryPoint" ]
+
+CMD ["com.example.fn.GetDiscountPool::handleRequest"]
---
+++ src/main/java/com/example/fn/GetDiscountPool.java
@@ -0,0 +1,141 @@
+package com.example.fn;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+//import com.cedarsoftware.util.io.JsonWriter;
+import java.sql.*;
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+
+public class GetDiscountPool {
+    private PoolDataSource poolDataSource;
+    private final String dbUser         = System.getenv().get("DB_USER");
+    private final String dbPassword     = System.getenv().get("DB_PASSWORD");
+    private final String dbUrl          = System.getenv().get("DB_URL") + System.getenv().get("DB_SERVICE_NAME") + "?TNS_ADMIN=/function/wallet";
+    //private final String clientCredPath = System.getenv().get("CLIENT_CREDENTIALS");
+    //private final String keyStorePasswd = System.getenv().get("KEYSTORE_PASSWORD");
+    //private final String truStorePasswd = System.getenv().get("TRUSTSTORE_PASSWORD");
+
+    public GetDiscountPool(){
+        System.err.println("Setting up pool data source");
+        //*********** FOR TESTING ONLY *************************************
+        System.err.println("ENV::" + dbUser);
+        System.err.println("ENV::" + dbPassword);
+        System.err.println("ENV::" + dbUrl);
+        poolDataSource = PoolDataSourceFactory.getPoolDataSource();
+        try {
+            poolDataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+            poolDataSource.setURL(dbUrl);
+            poolDataSource.setUser(dbUser);
+            poolDataSource.setPassword(dbPassword);
+            poolDataSource.setConnectionPoolName("UCP_POOL");
+        }
+        catch (SQLException e) {
+            System.out.println("Pool data source error!");
+            e.printStackTrace();
+        }
+        System.err.println("Pool data source setup...");
+    }
+
+    public static class Input {
+        public String demozone;
+        public String paymentMethod;
+        public String pizzaPrice;
+
+        public String toString() {
+            StringBuilder stb = new StringBuilder("{");
+            stb.append("'demozone':'").append(demozone).append("'");
+            stb.append("'paymentMethod':'").append(paymentMethod).append("'");
+            stb.append("'pizzaPrice':'").append(pizzaPrice).append("'");
+            stb.append("}");
+            return stb.toString();
+        }
+    }
+
+    public String handleRequest(Input pizzaData) {
+        String exitValues    = "SALIDA::";
+        ResultSet resultSet  = null;
+        Connection conn      = null;
+        float discount       = 0;
+
+        try {
+            String paymentMethod = pizzaData.paymentMethod.toUpperCase();
+            String demozone      = pizzaData.demozone.toUpperCase();
+            String pizzaPrice    = pizzaData.pizzaPrice;
+
+            //cast string input into a float
+            System.err.println("inside Discount Function gigis fn function!!! ");
+            float totalPaidValue  = Float.parseFloat(pizzaPrice);
+
+            System.setProperty("oracle.jdbc.fanEnabled", "false");
+
+            conn = poolDataSource.getConnection();
+            conn.setAutoCommit(false);
+
+            try {                
+                if (conn != null) {
+                    System.err.println("Connected to Oracle ATP DB Pool successfully");                
+                    //System.err.println("QUERY:: Driver getConnection");
+
+                    StringBuilder stb = new StringBuilder("SELECT NVL (");
+                    stb.append("(SELECT SUM(DISCOUNT) FROM CAMPAIGN WHERE ");
+                    stb.append("DEMOZONE LIKE ? ");
+                    stb.append("AND PAYMENTMETHOD LIKE ? ");
+                    stb.append("AND CURRENT_DATE BETWEEN DATE_BGN AND DATE_END+1 ");
+                    stb.append("AND MIN_AMOUNT <= ?)");
+                    stb.append(",0) as DISCOUNT FROM DUAL");
+                    PreparedStatement pstmt = conn.prepareStatement(stb.toString());
+
+                    pstmt.setString(1,demozone);
+                    pstmt.setString(2,paymentMethod);
+                    pstmt.setFloat(3,Float.parseFloat(pizzaPrice));
+                    
+                    /*********** FOR TESTING ONLY *************************************
+                    System.err.println("QUERY::    " + stb.toString());
+                    System.err.println("QUERY:: 1. " + demozone);
+                    System.err.println("QUERY:: 2. " + paymentMethod);
+                    System.err.println("QUERY:: 3. " + pizzaPrice);
+                    */
+                    
+                    System.err.println("[" + pizzaData.toString() + "] - Pizza Price before discount: " + totalPaidValue + "$");
+                    resultSet = pstmt.executeQuery();
+                    if (resultSet.next()){                                                
+                        discount = Float.parseFloat(resultSet.getString("DISCOUNT"))/100;                        
+                        if (discount > 0){
+                            //apply calculation to float eg: discount = 10%
+                            totalPaidValue -=  (totalPaidValue*discount);
+                            System.err.println("[" + pizzaData.toString() + "] - discount: " + resultSet.getString("DISCOUNT") + "%");
+                        }
+                        else
+                            System.err.println ("[" + pizzaData.toString() + "] - No Discount campaign for this payment! [0%]");
+                    }
+                    else {
+                        System.err.println ("[" + pizzaData.toString() + "] - No Discount campaign for this payment!");
+                    }
+                    System.err.println("[" + pizzaData.toString() + "] - total Pizza Price after discount: " + totalPaidValue + "$");
+                    exitValues = Float.toString(totalPaidValue);
+                }
+                else {
+                    System.err.println("[" + pizzaData.toString() + "] - Error: DB Connection Problem"); 
+                    throw new NullPointerException("DB Connection Problem : NULL!");
+                }
+            }     
+            catch (Exception ex) {
+                StringWriter errors = new StringWriter();
+                ex.printStackTrace(new PrintWriter(errors));                 
+                exitValues = pizzaData.toString() + " - Error: " + ex.toString() + "\n" + ex.getMessage() + errors.toString();                
+            }  
+            finally {
+                conn.close();
+            }
+        }
+        catch (Exception ex){
+            StringWriter errors = new StringWriter();
+            ex.printStackTrace(new PrintWriter(errors));                 
+            exitValues = pizzaData.toString() + " - Error: " + ex.toString() + "\n" + ex.getMessage() + errors.toString();;
+        }
+        finally{
+            return exitValues;
+        }
+    }
+}
